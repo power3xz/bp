205135056fc87f990be076990a63286c71faf88a
devj <power3xz@gmail.com>
2021-06-13 12:23:53 +0900

    add anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context, Result};
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -7,9 +8,13 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[derive(Debug)]
+struct CustomError(String);
+
+fn main() -> Result<()> {
     let args = Cli::from_args();
-    let content = std::fs::read_to_string(&args.path)?;
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("colud not read file `{}`", args.path.to_str().unwrap()))?;
     for line in content.lines() {
         println!("{}", line)
     }
