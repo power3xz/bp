027c4d577c8fa0b32658f1458ca359b70a980157
devj <power3xz@gmail.com>
2021-06-13 20:10:32 +0900

    add impl From<Arg> to Command

--- src/command.rs
+++ src/command.rs
@@ -1,6 +1,29 @@
+use std::env::Args;
+
 pub enum Command {
     Get,
     Add(String),
     Done(usize),
     Remove(usize),
+}
+
+impl From<Args> for Command {
+    fn from(args: Args) -> Self {
+        let arguments: Vec<String> = args.collect();
+        match arguments[1].as_str() {
+            "get" => Command::Get,
+            "add" => Command::Add(arguments[2].clone()),
+            "done" => Command::Done(
+                arguments[2]
+                    .parse::<usize>()
+                    .expect("error converting to int"),
+            ),
+            "remove" => Command::Remove(
+                arguments[2]
+                    .parse::<usize>()
+                    .expect("error converting to int"),
+            ),
+            _ => panic!("you must provide an accepted command"),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,10 @@ use std::env;
 use todo_list::TodoList;
 
 fn main() {
-    let arguments: Vec<String> = env::args().collect();
+    let command = Command::from(env::args());
     let mut todo_list = TodoList::new();
     todo_list.add_to_list("Say hi to CJ");
     todo_list.add_to_list("Say hi to ch");
-    let command = match arguments[1].as_str() {
-        "get" => Command::Get,
-        "add" => Command::Add(arguments[2].clone()),
-        "done" => Command::Done(
-            arguments[2]
-                .parse::<usize>()
-                .expect("error converting to int"),
-        ),
-        "remove" => Command::Remove(
-            arguments[2]
-                .parse::<usize>()
-                .expect("error converting to int"),
-        ),
-        _ => panic!("you must provide an accepted command"),
-    };
 
     match command {
         Command::Get => todo_list.print(),
