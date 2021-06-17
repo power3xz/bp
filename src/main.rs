82feb58d45b4379582e943d8923b6b02c72e8928
devj <power3xz@gmail.com>
2021-06-17 15:21:53 +0900

    split main function

--- src/main.rs
+++ src/main.rs
@@ -20,36 +20,37 @@ fn save(todo_list: &TodoList, data_path: &Path) -> std::io::Result<()> {
     Ok(())
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let command = Command::from(env::args());
-    let data_path = Path::new("data");
+fn do_command(command: Command, data_path: &Path) -> std::io::Result<()> {
     let mut todo_list = match data_path.exists() {
         true => TodoList::from(data_path),
         false => TodoList::new(),
     };
-
     match command {
-        Command::Get => todo_list.print(),
+        Command::Get => (),
         Command::Add(task) => {
             todo_list.add_to_list(&task);
-            todo_list.print();
             save(&todo_list, data_path)?;
         }
         Command::Describe(index, description) => {
             todo_list.add_description(index, description);
-            todo_list.print();
             save(&todo_list, data_path)?;
         }
         Command::Done(index) => {
             todo_list.mark_done(index);
-            todo_list.print();
             save(&todo_list, data_path)?;
         }
         Command::Remove(index) => {
             todo_list.remove_task(index);
-            todo_list.print();
             save(&todo_list, data_path)?;
         }
     };
+    todo_list.print();
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let command = Command::from(env::args());
+    let data_path = Path::new("./data");
+    do_command(command, data_path)?;
     Ok(())
 }
