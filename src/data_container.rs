3d55a3f8fe342620b8bcf052e2187b04ece226d3
devj <power3xz@gmail.com>
2021-06-17 21:45:16 +0900

    move save to data_container

--- src/data_container.rs
+++ src/data_container.rs
@@ -3,17 +3,26 @@ use std::fs;
 use std::io::Write;
 use std::path::Path;
 
-pub fn save(todo_list: &TodoList, data_path: &Path) -> std::io::Result<()> {
-    let mut f = match data_path.exists() {
-        true => fs::OpenOptions::new()
-            .write(true)
-            .truncate(true)
-            .open(data_path)?,
-        false => fs::File::create(data_path)?,
-    };
-    for item in &todo_list.list {
-        f.write_all(format!("{}\n", item.to_csv()).as_bytes())?;
+pub struct DataContainer<'a> {
+    data_path: &'a Path,
+}
+
+impl<'a> DataContainer<'a> {
+    pub fn new(data_path: &'a Path) -> Self {
+        Self { data_path }
     }
-    f.flush()?;
-    Ok(())
+    pub fn save(&self, todo_list: &TodoList) -> std::io::Result<()> {
+        let mut f = match self.data_path.exists() {
+            true => fs::OpenOptions::new()
+                .write(true)
+                .truncate(true)
+                .open(self.data_path)?,
+            false => fs::File::create(self.data_path)?,
+        };
+        for item in &todo_list.list {
+            f.write_all(format!("{}\n", item.to_csv()).as_bytes())?;
+        }
+        f.flush()?;
+        Ok(())
+    }
 }
