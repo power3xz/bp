f69bdb672324dbc205ae09032f74c1e0a474e39c
devj <power3xz@gmail.com>
2021-06-17 21:18:12 +0900

    split save function

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,12 @@
 mod command;
+mod data_container;
 mod todo_item;
 mod todo_list;
 
 use command::Command;
 use std::env;
-use std::fs;
-use std::io::Write;
 use std::path::Path;
 use todo_list::TodoList;
-
-fn save(todo_list: &TodoList, data_path: &Path) -> std::io::Result<()> {
-    let mut f = match data_path.exists() {
-        true => fs::OpenOptions::new()
-            .write(true)
-            .truncate(true)
-            .open(data_path)?,
-        false => fs::File::create(data_path)?,
-    };
-    for item in &todo_list.list {
-        f.write_all(format!("{}\n", item.to_csv()).as_bytes())?;
-    }
-    f.flush()?;
-    Ok(())
-}
 
 fn do_command(command: Command, data_path: &Path) -> std::io::Result<()> {
     let mut todo_list = match data_path.exists() {
@@ -33,19 +17,19 @@ fn do_command(command: Command, data_path: &Path) -> std::io::Result<()> {
         Command::Get => (),
         Command::Add(task) => {
             todo_list.add_to_list(&task);
-            save(&todo_list, data_path)?;
+            data_container::save(&todo_list, data_path)?;
         }
         Command::Describe(index, description) => {
             todo_list.add_description(index, description);
-            save(&todo_list, data_path)?;
+            data_container::save(&todo_list, data_path)?;
         }
         Command::Done(index) => {
             todo_list.mark_done(index);
-            save(&todo_list, data_path)?;
+            data_container::save(&todo_list, data_path)?;
         }
         Command::Remove(index) => {
             todo_list.remove_task(index);
-            save(&todo_list, data_path)?;
+            data_container::save(&todo_list, data_path)?;
         }
     };
     todo_list.print();
